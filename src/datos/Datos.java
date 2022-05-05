a05fd9b7ce043475aa35801f2e30b787f73ae926
Kevin Chávez <Kevin Chvez@KEVINS-LAPTOP>
2022-05-05 10:29:09 -0600

    datos del alumno

---
+++ src/datos/Datos.java
@@ -0,0 +1,11 @@
+package datos;
+
+public class Datos {
+
+    public static void main(String[] args) {
+        System.out.println("***INFORMACION DEL ALUMNO***");
+        System.out.printf("%s%s\n ", "El nombre del alumno es: ", info.getNombre());
+        System.out.printf("%s%s\n ", "La carrera del alumno es: ", info.getCarrera());
+        System.out.printf("%s%s\n ", "La matricula del alumno es: ", info.getMatricula());
+    }
+}
---
+++ src/datos/info.java
@@ -0,0 +1,35 @@
+package datos;
+
+public class info extends Object{
+    String nombre, carrera, matricula;
+
+    public info(String nombre, String carrera, String matricula) {
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
